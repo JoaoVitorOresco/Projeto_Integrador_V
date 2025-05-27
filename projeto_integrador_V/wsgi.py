2c47f13859dc26546ea4ee90772434eb3f05456f
JoaoVitorOresco <jvoresco76@gmail.com>
2025-05-27 20:31:54 -0300

    remove de duplicidade

--- projeto_integrador_V/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_integrador_V.settings')
-
-application = get_wsgi_application()
